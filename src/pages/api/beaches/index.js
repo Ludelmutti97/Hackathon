8a0b8dce1a77384306c170c26a536c2429c68b6b
Luanna Delmuti <ludelmutti@gmail.com>
2023-06-14 12:29:27 +0100

    adasj

---
+++ src/pages/api/beaches/index.js
@@ -0,0 +1,16 @@
+import { getBeaches } from "@/server/services/beaches";
+
+
+// Todas as praias
+export default async function handler(req, res) {
+    try {
+        if (req.method === "GET") {
+            const beaches = await getBeaches();
+            return res.status(200).json(beaches) ;  
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
